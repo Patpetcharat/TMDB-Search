ef44b822d03a43a01e1b9d348842f13b2c7c10ae
Patrick Krabeepetcharat <Patpetcharat@gmail.com>
2016-03-10 21:16:19 -0800

    Removed isomorphic rendering for now, since it conflicted with Greensock

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import path from 'path'
 import compression from 'compression'
-import React from 'react'
-import { renderToString } from 'react-dom/server'
-import { match, RouterContext } from 'react-router'
-import routes from './src/scripts/routes'
+// import React from 'react'
+// import { renderToString } from 'react-dom/server'
+// import { match, RouterContext } from 'react-router'
+// import routes from './src/scripts/routes'
 
 var app = express()
 
@@ -16,40 +16,43 @@ app.use(express.static('public'));
 
 // send all requests to index.html so browserHistory works
 app.get('*', (req, res) => {
-	match({ routes, location: req.url }, (err, redirect, props) => {
-		if (err) {
-			res.status(500).send(err.message)
-		} else if (redirect) {
-			res.redirect(redirect.pathname + redirect.search)
-		} else if (props) {
-			// hey we made it!
-			const appHtml = renderToString(<RouterContext {...props}/>)
-			res.send(renderPage(appHtml))
-		} else {
-			res.status(404).send('Not Found')
-		}
-	})
+	//res.send('index.html');
+	res.sendFile(path.join(__dirname + '/index.html'));
+
+	// match({ routes, location: req.url }, (err, redirect, props) => {
+	// 	if (err) {
+	// 		res.status(500).send(err.message)
+	// 	} else if (redirect) {
+	// 		res.redirect(redirect.pathname + redirect.search)
+	// 	} else if (props) {
+	// 		// hey we made it!
+	// 		const appHtml = renderToString(<RouterContext {...props}/>)
+	// 		res.send(renderPage(appHtml))
+	// 	} else {
+	// 		res.status(404).send('Not Found')
+	// 	}
+	// })
 })
 
-function renderPage(appHtml) {
-	return `
-		<!DOCTYPE html>
-		<html>
-		<head>
-			<title>Hello, React Isomornic</title>
+// function renderPage(appHtml) {
+// 	return `
+// 		<!DOCTYPE html>
+// 		<html>
+// 		<head>
+// 			<title>Hello, React Isomornic</title>
 			
-			<link rel="stylesheet" type="text/css" href="/styles/app.css">
-			<link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
+// 			<link rel="stylesheet" type="text/css" href="/styles/app.css">
+// 			<link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
 
-			<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
-		</head> 	
-		<body>
-			<div id="app">${appHtml}</div>
-			<script type="text/javascript" src="/scripts/bundle.js"></script>
-		</body>
-		</html>
-	`
-}
+// 			<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
+// 		</head> 	
+// 		<body>
+// 			<div id="app">${appHtml}</div>
+// 			<script type="text/javascript" src="/scripts/bundle.js"></script>
+// 		</body>
+// 		</html>
+// 	`
+// }
 
 var PORT = process.env.PORT || 8080
 app.listen(PORT, function() {
