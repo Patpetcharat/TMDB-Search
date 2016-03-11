a034a9d68a749f678bfc7c9b674bdd6ddd8a076d
Patrick Krabeepetcharat <Patpetcharat@gmail.com>
2016-03-10 16:25:53 -0800

    trace results

--- src/scripts/components/Search.js
+++ src/scripts/components/Search.js
@@ -1,11 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import TweenMax from 'gsap'
+import $ from 'jquery'
 
 export default class SearchBar extends React.Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			data: {
+				results: []
+			}
+		}
+	}
+
 	componentDidMount(){
+		// Get the DOM object for animating with Greensock
 		this.fullPageContainer = ReactDOM.findDOMNode(this.refs.fullPageContainer);
-		console.log('fullPageContainer:', this.fullPageContainer);	
+
+		// Make the API request
+		const { searchTerm, pageNumber } = this.props.params;
+
+		const api_url = "http://api.themoviedb.org/3/search/movie?api_key=42b3e60b6636f50062f6d3579100d83f&query=";
+
+		let request_url = api_url + encodeURIComponent(searchTerm);
+
+		if(pageNumber){
+			request_url += "&page=";
+			request_url += pageNumber;
+		}
+
+		$.get(request_url, (res)=>{
+			this.state.data = res;
+			this.setState(this.state);
+
+			console.log('data:', this.state);
+		});
 	}
 
 	componentWillEnter(callback){
@@ -17,22 +47,26 @@ export default class SearchBar extends React.Component {
 	}
 
 	componentWillLeave(callback){
-		console.log('will leave');
-
 		TweenMax.to(this.fullPageContainer, 1, {opacity:0, onComplete:callback});
 	}
 	render() {
-		const { searchTerm, pageNumber } = this.props.params
+		// TEST
+		const { searchTerm, pageNumber } = this.props.params 
+
+		let resultNodes = this.state.data.results.map(movie => {
+			return (
+				<div className="movie" key={movie.id}>
+					<h2>{movie.title}</h2>
+				</div>
+			);
+		});
+
 		return (
 			<div ref="fullPageContainer" className="fullPageContainer">
 				<h2>searchTerm: {searchTerm}</h2>
 				<h2>pageNumber: {pageNumber}</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at nisi tristique, faucibus leo faucibus, iaculis justo. Sed erat velit, rutrum sed risus sed, lacinia sodales eros. Aenean faucibus magna a quam tempor, quis accumsan mi rhoncus. Aliquam tincidunt suscipit velit, sit amet eleifend dui interdum eget. Sed imperdiet ligula sed leo dapibus, ac sagittis odio vehicula. Pellentesque feugiat purus vitae elit convallis, vitae fermentum justo faucibus. Aliquam tincidunt dolor tortor, sed ullamcorper erat dignissim id. Phasellus ultricies urna nunc, vel euismod ligula maximus et. Nulla facilisi. Nullam a sem viverra, elementum tellus a, sollicitudin massa. Phasellus quis ornare ligula.
-				</p>
-				<p>Vivamus blandit nec odio at viverra. Donec sollicitudin erat sit amet arcu gravida faucibus. Phasellus luctus sed est ac scelerisque. Donec finibus libero ut enim luctus, id maximus lacus suscipit. Duis iaculis ante vitae commodo aliquam. Duis nec laoreet eros. Vestibulum nulla dolor, volutpat et vestibulum ut, condimentum at ligula. Aenean id nisl vitae risus semper elementum. Mauris id libero at elit pulvinar ornare.
-				</p>
-				<p>Nunc ac dolor ut massa finibus lobortis. Morbi lectus mauris, cursus et sollicitudin dignissim, iaculis sit amet ligula. Duis est eros, interdum at quam a, sollicitudin accumsan ex. Duis nisi nisl, convallis vitae laoreet ac, viverra vitae neque. Vivamus tempus, sapien in tempus convallis, nisl erat ornare ex, suscipit ornare purus sem vel sem. Nulla placerat molestie commodo. Quisque ornare ante in ex cursus, blandit laoreet lorem tempor. Sed dictum rutrum velit in suscipit. Maecenas in egestas mauris. Nunc eget dui sit amet mauris feugiat varius. Phasellus id ipsum dictum, rutrum lacus vel, euismod nulla.
-							</p>
+
+				{resultNodes}
 			</div>
 		)
 	}
